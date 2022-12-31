56363562fcd312e3c71660c94317eda5bbbe5817
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2022-12-31 14:27:05 -0500

    Clase Cliente

---
+++ src/main/java/clientes/Cliente.java
@@ -0,0 +1,76 @@
+package clientes;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Cliente {
+    private String cedula;
+    private String nombres;
+    private String apellidos;
+    private LocalDate fecha;
+    private char sexo;
+    private String telefono;
+    private String nombreContacto;
+    private String telefonoContacto;
+    private String correoElectronico;
+    private String direccion;
+
+    public Cliente(String cedula, String nombres, String apellidos, String fecha, char sexo, String telefono, String nombreContacto,
+                   String telefonoContacto, String correoElectronico, String direccion) {
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecha = stringALocalDate(fecha);
+        this.sexo = sexo;
+        this.telefono = telefono;
+        this.nombreContacto = nombreContacto;
+        this.telefonoContacto = telefonoContacto;
+        this.correoElectronico = correoElectronico;
+        this.direccion = direccion;
+
+    }
+
+    public static LocalDate stringALocalDate(String fecha) {
+        return LocalDate.parse(fecha, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getNombreContacto() {
+        return nombreContacto;
+    }
+
+    public String getTelefonoContacto() {
+        return telefonoContacto;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+}
---
+++ src/test/java/clientes/ClienteTest.java
@@ -0,0 +1,46 @@
+package clientes;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.*;
+
+public class ClienteTest {
+    @Test
+    public void given_parameters_when_new_client_with_parameters_then_ok(){
+        String cedula = "1725292542";
+        String nombres = "Angelo Alexandro";
+        String apellidos = "Abad Abarca";
+        String fecha = "15-08-1997";
+        char sexo = 'M';
+        String telefono = "0963870957";
+        String nombreContacto = "Diana Abad";
+        String telefonoContacto = "0964255255";
+        String correoElectronico = "abad14@gmail.com";
+        String direccion = "Av.Maldonado";
+
+
+
+        Cliente cli=new Cliente(cedula, nombres,
+                apellidos, fecha,
+                sexo, telefono,
+                nombreContacto, telefonoContacto,
+                correoElectronico, direccion);
+
+
+        assertNotNull(cli);
+        assertEquals(cedula,cli.getCedula());
+        assertEquals(nombres,cli.getNombres());
+        assertEquals(apellidos,cli.getApellidos());
+        assertEquals(LocalDate.parse(fecha, DateTimeFormatter.ofPattern("dd-MM-yyyy")),cli.getFecha());
+        assertEquals(sexo,cli.getSexo());
+        assertEquals(telefono,cli.getTelefono());
+        assertEquals(nombreContacto,cli.getNombreContacto());
+        assertEquals(telefonoContacto,cli.getTelefonoContacto());
+        assertEquals(correoElectronico,cli.getCorreoElectronico());
+        assertEquals(direccion,cli.getDireccion());
+
+    }
+}
