c241e15e2e84c9451dca5f95b757c2f437f1b0a9
KennethUlloa <ulloakenth@gmail.com>
2022-12-31 17:16:23 -0500

    Refactorización de las pruebas

--- src/test/java/validacion/ValidadorCedulaTest.java
+++ src/test/java/validacion/ValidadorCedulaTest.java
@@ -1,46 +1,33 @@
 package validacion;
 
 import clientes.Cliente;
+import clientes.excepciones.ErrorCedula;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ValidadorCedulaTest {
 
-    @Test
-    public void given_new_client_when_not_valid_cedula_then_error(){
+    @Test(expected = ErrorCedula.class)
+    public void given_new_client_when_not_valid_cedula_then_error() throws ErrorCedula {
         Cliente c= new Cliente("1514","Angelo Alexandro",
                 "Abad Abarca","15-08-1997",
                 'M',"0963870957",
                 "Diana Abad","0964255255",
                 "abad14@gmail.com","Av.Maldonado");
-        ValidadorCedula val=new ValidadorCedula(c.getCedula());
-        Exception error=null;
-        try {
-            val.validar();
-        } catch (Exception e) {
-            error=e;
-        }
-
-        assertNotNull(error);
+        ValidadorCedula val = new ValidadorCedula(c.getCedula());
+        val.validar();
     }
 
     @Test
-    public void given_new_client_when_valid_cedula_then_ok(){
+    public void given_new_client_when_valid_cedula_then_ok() throws ErrorCedula {
         Cliente c= new Cliente("1753953866","Angelo Alexandro",
                 "Abad Abarca","15-08-1997",
                 'M',"0963870957",
                 "Diana Abad","0964255255",
                 "abad14@gmail.com","Av.Maldonado");
         ValidadorCedula val=new ValidadorCedula(c.getCedula());
-        Exception error=null;
-        try {
-            val.validar();
-        } catch (Exception e) {
-            error=e;
-        }
-
-        assertNull(error);
+        val.validar();
     }
 
 }
