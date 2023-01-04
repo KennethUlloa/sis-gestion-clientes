e9b2339715714f3b5123698e84e6957ade4e1cd1
marioVillamar <mjvc98@hotmail.es>
2023-01-04 15:40:19 -0500

    -Agregacion de la opcion de creacion de usuarios del sistema al menu -Afgegracion del paquete usuario

---
+++ src/main/java/usuarios/Usuario.java
@@ -0,0 +1,36 @@
+package usuarios;
+
+public class Usuario {
+    String usuario;
+    String contrasena;
+
+    public Usuario(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "   usuario='" + usuario + "'\n" +
+                "   contrasena='" + contrasena + "'\n" +
+                '}';
+    }
+}
+
