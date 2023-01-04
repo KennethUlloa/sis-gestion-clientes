b8b582677989ddf0c7fe4cc7b55694aaae23f21c
marioVillamar <mjvc98@hotmail.es>
2023-01-04 15:39:27 -0500

    -Agregacion de la opcion de creacion de usuarios del sistema al menu -Afgegracion del paquete usuario

--- src/main/java/menu/MenuPrincipal.java
+++ src/main/java/menu/MenuPrincipal.java
@@ -1,5 +1,6 @@
 package menu;
 
+import menu.autenticacion.OpcionModuloRegistroUsuarios;
 import menu.clientes.OpcionModuloClientes;
 
 public class MenuPrincipal extends Menu {
@@ -7,16 +8,19 @@ public class MenuPrincipal extends Menu {
     public MenuPrincipal() {
         super("MENU PRINCIPAL");
         agregarOpcion(new OpcionModuloClientes());
+        agregarOpcion(new OpcionModuloRegistroUsuarios());
         agregarOpcion(new OpcionSalir());
+
     }
 
     @Override
     public boolean cerrarMenu(int op) {
-        return op == 2;
+        return op == 3;
     }
 
     @Override
     public boolean validarOpcion(int opcion) {
         return opcion-1 >= 0 && opcion-1 < contarOpciones();
     }
+
 }
